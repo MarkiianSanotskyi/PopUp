da1adb7ca97ab329d231d2490404635e9dad7b89
Markiian Sanotskyi <marksanockiy@gmail.com>
2020-08-14 10:20:52 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+/*placeholder*/
+$(document).ready(function(){
+        $.Placeholder.init({ color : "#939598" });
+ });
+
+
+(function($) {
+$(function() {
+
+  $('input, select').styler();
+
+});
+})(jQuery);
